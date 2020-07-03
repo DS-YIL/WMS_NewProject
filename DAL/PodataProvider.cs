a6050260864de30631f610e001da0d408087be96
464-3268 <ramesh.kumar@in.yokogawa.com>
2020-07-03 13:57:37 +0530

    api changes

--- DAL/PodataProvider.cs
+++ DAL/PodataProvider.cs
@@ -2552,11 +2552,12 @@ namespace WMS.DAL
 				try
 				{
 					string query = WMSResource.getitemlocationList.Replace("#materialid", material);
-
-
 					await pgsql.OpenAsync();
-					return await pgsql.QueryAsync<IssueRequestModel>(
+					var data = await pgsql.QueryAsync<IssueRequestModel>(
 					  query, null, commandType: CommandType.Text);
+					  data = data.OrderByDescending(o => o.createddate);
+					return data;
+					
 
 
 				}
