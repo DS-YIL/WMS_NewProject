c9ef90a400bf1d86a83df32c15889360118c32f0
lakshmiprasanna44 <nlakshmiprasanna44@gmail.com>
2021-01-12 13:25:56 +0530

    staging code changes for po base tables

--- Common/Configurations.cs
+++ Common/Configurations.cs
@@ -7,40 +7,45 @@ using System.Threading.Tasks;
 
 namespace WMS.Common
 {
-    public class Configurations
-    {
-        public readonly string _SQLconnectionString = string.Empty;
-        public readonly string _PostgreconnectionString = string.Empty;
-        public readonly string _EmailLinkUrl = string.Empty;
-        public Configurations()
-        {
-            var configurationBuilder = new ConfigurationBuilder();
-            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
-            configurationBuilder.AddJsonFile(path, false);
+	public class Configurations
+	{
+		public readonly string _SQLconnectionString = string.Empty;
+		public readonly string _PostgreconnectionString = string.Empty;
+		public readonly string _EmailLinkUrl = string.Empty;
+		public readonly string _FilePath = string.Empty;
+		public Configurations()
+		{
+			var configurationBuilder = new ConfigurationBuilder();
+			var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+			configurationBuilder.AddJsonFile(path, false);
 
-            var root = configurationBuilder.Build();
-            _SQLconnectionString = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+			var root = configurationBuilder.Build();
+			_SQLconnectionString = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
 
-            _PostgreconnectionString = root.GetSection("ConnectionStrings").GetSection("PostgresConnection").Value;
-            _EmailLinkUrl = root.GetSection("ConnectionStrings").GetSection("EmailLinkUrl").Value;
+			_PostgreconnectionString = root.GetSection("ConnectionStrings").GetSection("PostgresConnection").Value;
+			_EmailLinkUrl = root.GetSection("ConnectionStrings").GetSection("EmailLinkUrl").Value;
+			_FilePath = root.GetSection("ConnectionStrings").GetSection("LiveFilePath").Value;
+			var appSetting = root.GetSection("ApplicationSettings");
 
-            var appSetting = root.GetSection("ApplicationSettings");
+		}
 
-        }
+		public string SQLConnectionString
+		{
+			get => _SQLconnectionString;
+		}
 
-        public string SQLConnectionString
-        {
-            get => _SQLconnectionString;
-        }
+		public string PostgresConnectionString
+		{
+			get => _PostgreconnectionString;
+		}
+		public string EmailLinkUrl
+		{
+			get => _EmailLinkUrl;
+		}
 
-        public string PostgresConnectionString
-        {
-            get => _PostgreconnectionString;
-        }
-        public string EmailLinkUrl
-        {
-            get => _EmailLinkUrl;
-        }
-
-    }
+		public string FilePath
+		{
+			get => _FilePath;
+		}
+	}
 }
