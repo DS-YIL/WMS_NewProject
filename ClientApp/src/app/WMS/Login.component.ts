10a726aae7c3d80a197ec6513065887e2ff46072
lakshmiprasanna44 <nlakshmiprasanna44@gmail.com>
2021-05-07 16:07:45 +0530

    login missed code

--- ClientApp/src/app/WMS/Login.component.ts
+++ ClientApp/src/app/WMS/Login.component.ts
@@ -157,6 +157,7 @@ export class LoginComponent implements OnInit {
                   });
                   if (dt1.length > 0) {
                     this.employee.roleid = this.LoginForm.value.roleid;
+                    this.employee.isdelegatemember = this.AcessNameList.filter(li => li.roleid == this.employee.roleid)[0].isdelegatemember;
                     this.employee.plantid = this.AcessNameList[0].plantid;
                     this.wmsService.getpagesbyrole(parseInt(this.employee.roleid)).subscribe(datax => {
                       this.pagelist = datax;
